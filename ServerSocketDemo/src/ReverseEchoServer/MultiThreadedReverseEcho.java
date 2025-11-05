6288451c58452c6e380f423962c87bd158325732
Vishesh Raghuvanshi <visheshkumarraghuvanshi@gmail.com>
2025-11-05 21:47:34 +0530

    Socket, ServerSocket and DatagramSocket

---
+++ ServerSocketDemo/src/ReverseEchoServer/MultiThreadedReverseEcho.java
@@ -0,0 +1,68 @@
+package ReverseEchoServer;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+// For Multiple Users
+public class MultiThreadedReverseEcho extends Thread {
+    Socket stk;
+    public MultiThreadedReverseEcho(Socket s) {
+        stk = s;
+    }
+    public void run() {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+            PrintStream ps = new PrintStream(stk.getOutputStream());
+
+            StringBuilder sb;
+            String msg;
+            do {
+                msg = br.readLine();
+                sb = new StringBuilder(msg);
+                sb.reverse();
+                msg = sb.toString();
+                ps.println(msg);
+            } while (!msg.equals("dne"));
+            stk.close();
+        } catch (Exception e) {}
+    }
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(2000);
+
+        Socket stk;
+        MultiThreadedReverseEcho re;
+        int count = 0;
+        do {
+            stk = ss.accept();
+            System.out.println("Client Connected " + ++count);
+            re = new MultiThreadedReverseEcho(stk);
+            re.start();
+        } while(true);
+
+
+    }
+}
+
+class MultiThreadedClient {
+    public static void main(String[] args) throws Exception {
+        Socket stk = new Socket("localhost",2000);
+
+        BufferedReader keyb = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(stk.getInputStream()));
+        PrintStream ps = new PrintStream(stk.getOutputStream());
+
+
+        String msg;
+        do {
+            msg = keyb.readLine();
+            ps.println(msg);
+            msg = br.readLine();
+            System.out.println("From Server " + msg);
+
+        } while (!msg.equals("dne"));
+        stk.close();
+    }
+}
